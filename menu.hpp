9f8d7bbc9710620d89ec460aad3eb899420c4e8b
123wesley <40710748+123wesley@users.noreply.github.com>
2019-01-05 02:52:06 +0800

    Add files via upload

---
+++ menu.hpp
@@ -0,0 +1,23 @@
+#include <SFML/Graphics.hpp>
+using namespace sf;
+
+const int MAX_NUMBER_OF_ITEMS = 3;
+
+class Menu
+{
+public:
+    Menu(float width, float height);
+    ~Menu();
+    
+    void draw(RenderWindow &window);
+    void MoveUp();
+    void MoveDown();
+    int GetPressItem(){return selectedItemIndex;};
+
+private:
+    int selectedItemIndex;
+    Font font;
+    Text title;
+    Text menu [MAX_NUMBER_OF_ITEMS];
+    
+};
